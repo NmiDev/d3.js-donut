b646b3f0d7770993bed1ce3c7986fd941c85f428
NmiDev <nicolas.missiaen@gmail.com>
2020-02-20 11:36:16 +0100

    Graph animation with tweens

--- graph.js
+++ graph.js
@@ -27,8 +27,48 @@ const pie = d3.pie()
     .sort(null)
     .value(d => d.cost);
 
+// Path arc generator
+const arcPath = d3.arc()
+    .outerRadius(dims.chartRadius)
+    .innerRadius(dims.chartRadius/2);
+
+// Ordinal scale initialisation
+const colour = d3.scaleOrdinal(d3['schemeSet1']);
+
+// Tween
+function arcTweenEnter(d) {
+    const i = d3.interpolate(d.endAngle, d.startAngle);
+
+    return function(t){
+        d.startAngle = i(t);
+        return arcPath(d);
+    }
+}
+
+function arcTweenExit(d) {
+    const i = d3.interpolate(d.startAngle, d.endAngle);
+
+    return function(t){
+        d.startAngle = i(t);
+        return arcPath(d);
+    }
+}
+
+function arcTweenUpdate(d) {
+    // Interpolate the two objects (all keys/value by default)
+    const i = d3.interpolate(this._current, d);
+    // Update the current prop with the updated data for next cycle
+    this._current = d;
+
+    return function(t) {
+        return arcPath(i(t));
+    }
+}
+
 // Update function
 const update = (data) => {
+    // Scale update with data
+    colour.domain(data.map(item => item.name))
     // Angles data
     const angles = pie(data);
     // Join data to path element
@@ -37,20 +77,32 @@ const update = (data) => {
     // Remove unnecessary shapes using the exit selection
     paths
         .exit()
+        .transition()
+        .duration(750)
+            .attrTween('d', arcTweenExit)
         .remove();
 
     // Update current shapes in the DOM
     paths
-        .attr('d', arcPath);
+        .attr('d', arcPath)
+        .transition()
+        .duration(750)
+            .attrTween('d', arcTweenUpdate);
 
     // Append the enter selection ti the DOM
     paths
         .enter()
         .append('path')
             .attr('class', 'arc')
-            .attr('d', arcPath)
             .attr('stroke', 'white')
-            .attr('stroke-width', 3);
+            .attr('stroke-width', 3)
+            .attr('fill', d => colour(d.data.name))
+            .each(function(d) {
+                this._current = d;
+            })
+            .transition()
+            .duration(750)
+                .attrTween('d', arcTweenEnter);
 
 }
 
@@ -88,10 +140,4 @@ db.collection('expenses').onSnapshot((snapshot) => {
     });
     update(data);
 });
-
-
-// Path arc generator
-const arcPath = d3.arc()
-    .outerRadius(dims.chartRadius)
-    .innerRadius(dims.chartRadius/2);
 
